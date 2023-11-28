0b96544d3ecd16a7bffda2132b573378c40cef10
Viet Anh Nguyen <vietanh.dev@gmail.com>
2023-11-28 23:22:41 +0700

    Update dataset

--- dnn_models/copy_data.sh
+++ dnn_models/copy_data.sh
@@ -1,5 +1,6 @@
 mkdir -p training/datasets/chessai/COCO/train
 mkdir -p training/datasets/chessai/COCO/val
+mkdir -p training/datasets/chessai/COCO/annotations
 cp data_preparation/data/train.json training/datasets/chessai/COCO/annotations/train.json
 cp data_preparation/data/val.json training/datasets/chessai/COCO/annotations/val.json
 cp -r data_preparation/data/combined_data/* training/datasets/chessai/COCO/train
